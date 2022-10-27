40273aac95bbae8192476410e1f832f9b8f83473
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-10-27 17:48:24 -0300

    incrementando css

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import React from 'react'
+ 
+import { BrowserRouter } from  'react-router-dom' // essa importação mudou
+import Menu from '../components/Menu'
+import Content from '../components/Content'
+ 
+
+ 
+const App = props => (
+    <div className="App">
+        <BrowserRouter> 
+            <Menu/>
+            <Content/>
+        </BrowserRouter>
+    </div>
+)
+ 
+export default App
